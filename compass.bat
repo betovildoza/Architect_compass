c590c56499abbdc8de3252fc886d2c335c44b905
Beto Vildoza <betovildoza@gmail.com>
2026-04-11 00:14:25 -0300

    cli roadmap included

--- compass.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Reemplaza la ruta de abajo por la carpeta real donde guardaste el script
-:: Asegurate de que termine en \architect_compass.py
-set SCRIPT_PATH="C:\herramientas\Architect_compass\architect_compass.py"
-
-python %SCRIPT_PATH%
-pause
